911e80b49685d9474333b1c9e5246b2c3641134c
Jahaziel Uko <jaybeelearn@gmail.com>
2023-05-31 23:22:19 +0100

    remove some line causing deployment error again done a

--- src/layout/ProductLayout.jsx
+++ src/layout/ProductLayout.jsx
@@ -3,7 +3,7 @@ import { FaChevronRight } from "react-icons/fa";
 import { Link, Outlet } from "react-router-dom";
 import data from "../assets/Data";
 
-import "../styling/Productlayout.css";
+import "../styling/Productlayout";
 import Content from "../Contents";
 
 function ProductLayout() {
